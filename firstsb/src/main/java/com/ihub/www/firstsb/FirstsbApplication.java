459f606831e2a3f7d1eb6c48aebf0a1adf311d41
Vijaya Priskilla <vijayav6924@gmail.com>
2024-05-30 10:16:05 +0530

    Spring Boot

---
+++ firstsb/src/main/java/com/ihub/www/firstsb/FirstsbApplication.java
@@ -0,0 +1,14 @@
+package com.ihub.www.firstsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstsbApplication.class, args);
+		System.out.println("I Love Springboot");
+	}
+
+}
---
+++ firstsb/src/test/java/com/ihub/www/firstsb/FirstsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihub.www.firstsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
